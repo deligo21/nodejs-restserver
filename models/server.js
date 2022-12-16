581f8c79e43dd314289decff12c9ef04c532f475
Rodrigo Rojas <84647769+deligo21@users.noreply.github.com>
2022-12-15 21:17:57 -0400

    Update server.js

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ import router from '../routes/usuarios.js';
 class Server{
     constructor(){
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         this.usuariosPath = '/api/usuarios'
 
         //Middlewares: funciones que anaden otras funcionalidades al webserver
